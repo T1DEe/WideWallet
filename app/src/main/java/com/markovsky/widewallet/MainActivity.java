67eeba56ef7c3bf73f890a4189c1ef8097bde5a4
T1DEe <artemexe2013@gmail.com>
2019-01-31 01:59:58 +0300

    btnAdd bug fixes

--- app/src/main/java/com/markovsky/widewallet/MainActivity.java
+++ app/src/main/java/com/markovsky/widewallet/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -30,28 +31,38 @@ public class MainActivity extends Activity {
             public void onClick(View v) {
                 LinearLayout createField = findViewById(R.id.LLforCreatedViews);
 
-                // Layout для каждой записи(строки)
-                LinearLayout createRow = new LinearLayout(createField.getContext());
-                createRow.setOrientation(LinearLayout.HORIZONTAL);
-                LinearLayout.LayoutParams newRowParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-                createRow.setLayoutParams(newRowParams);
+                if (productName.getText().toString().equals("") && productPrice.getText().toString().equals(""))
+                    Toast.makeText(createField.getContext(), R.string.errorEmptyString, Toast.LENGTH_LONG).show();
+                else
+                {
+                    // горизонтальный LinearLayout для каждой записи(строки)
+                    LinearLayout createRow = new LinearLayout(createField.getContext());
+                    createRow.setOrientation(LinearLayout.HORIZONTAL);
+                    LinearLayout.LayoutParams newRowParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+                    createRow.setLayoutParams(newRowParams);
 
+                    // Создание новой записи
+                    TextView newProductName = new TextView(createField.getContext());
+                    LinearLayout.LayoutParams newProductNameParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                    newProductNameParams.weight = 5;
 
-                // Настройка новой записи
-                TextView newProductName = new TextView(createField.getContext());
-                newProductName.setText(productName.getText().toString());
-                LinearLayout.LayoutParams newProductNameParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-                newProductNameParams.weight = 5;
+                    TextView newProductPrice = new TextView(createField.getContext());
+                    LinearLayout.LayoutParams newProductPriceParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                    newProductPriceParams.weight = 2;
 
-                TextView newProductPrice = new TextView(createField.getContext());
-                newProductPrice.setText(productPrice.getText().toString());
-                LinearLayout.LayoutParams newProductPriceParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-                newProductPriceParams.weight = 2;
+                    newProductName.setText(productName.getText().toString());
+                    newProductPrice.setText(productPrice.getText().toString());
 
-                createRow.addView(newProductName, newProductNameParams);
-                createRow.addView(newProductPrice, newProductPriceParams);
+                    // Отображение новой записи
+                    createRow.addView(newProductName, newProductNameParams);
+                    createRow.addView(newProductPrice, newProductPriceParams);
 
-                createField.addView(createRow, newRowParams);
+                    createField.addView(createRow, newRowParams);
+
+                    // Обнуление полей ввода
+                    productName.setText("");
+                    productPrice.setText("");
+                }
             }
         });
 
